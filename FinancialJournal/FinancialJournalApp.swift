f8d547825ce8c109d34b445a7677514ee7120d99
Zlobrynya <zlobrynya@gmail.com>
2022-04-09 11:48:33 +0300

    First commit

---
+++ FinancialJournal/FinancialJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialJournalApp.swift
+//  FinancialJournal
+//
+//  Created by Nikita Nikitin on 09.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct FinancialJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
