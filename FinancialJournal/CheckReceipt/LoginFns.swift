9a9bc249be2a0078d552df69ef2cf967a06e0ad7
Zlobrynya <zlobrynya@gmail.com>
2022-04-09 15:52:22 +0300

    Add SwiftGen

--- FinancialJournal/CheckReceipt/LoginFns.swift
+++ FinancialJournal/CheckReceipt/LoginFns.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct LoginFns: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
+            .foregroundColor(Colors.testColor.color)
     }
 }
 
