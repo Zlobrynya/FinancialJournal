e38e11f0aec76e92a8d0a4bc1b7f46a550049780
Zlobrynya <zlobrynya@gmail.com>
2022-04-09 15:51:22 +0300

    Add WebView and empty LoginFns

---
+++ FinancialJournal/CheckReceipt/LoginFns.swift
@@ -0,0 +1,20 @@
+//
+//  LoginFns.swift
+//  FinancialJournal
+//
+//  Created by Nikita Nikitin on 09.04.2022.
+//
+
+import SwiftUI
+
+struct LoginFns: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginFns_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginFns()
+    }
+}
